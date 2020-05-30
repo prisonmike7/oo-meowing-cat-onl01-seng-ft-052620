5562c5f30e49bcb06892a8e45b85f6287cf8ba1f
Michael_Harris <mwharris7@gmail.com>
2020-05-30 08:15:30 -0700

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,49 @@
-## code your solution here. 
+class Cat
+    
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+
+end
+
+maru = Cat.new
+maru.name = "Maru"
+ 
+maru.name
+# => "Maru"
+ 
+maru.meow
+# "meow!"
+# => nil
+
+# michael = Person.new("Prison Mike", "Full-Stack Web Developer")
+# puts michael.name
+# puts michael.job
+
+# class Cat
+    
+#     # def initialize(name, job)
+#     #     @name = name
+#     #     @job = job
+#     # end
+    
+#     def name
+#         @name
+#     end
+
+#     def name=(new_name)
+#         @name = new_name
+#     end
+
+#     def job
+#         @job
+#     end
+
+#     def job=(new_job)
+#         @job = new_job
+#     end
+
+# end
+ 
